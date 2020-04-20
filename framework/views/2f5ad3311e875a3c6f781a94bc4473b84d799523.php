5c39148f1a38c4cc0601b8462c4944cbb890f5b8
jescalante02 <55394334+jescalante02@users.noreply.github.com>
2020-04-19 21:23:31 -0500

    Add files via upload

---
+++ framework/views/2f5ad3311e875a3c6f781a94bc4473b84d799523.php
@@ -0,0 +1,3 @@
+<?php echo e($slot); ?>
+
+<?php /**PATH C:\xampp1\php\docs\laravelAuth2\vendor\laravel\framework\src\Illuminate\Mail/resources/views/text/footer.blade.php ENDPATH**/ ?>
---
+++ framework/views/e96ee119e87cb2dfdc2ab4463e90a6ad44b51a8b.php
@@ -0,0 +1,3 @@
+<?php echo e($slot); ?>: <?php echo e($url); ?>
+
+<?php /**PATH C:\xampp1\php\docs\laravelAuth2\vendor\laravel\framework\src\Illuminate\Mail/resources/views/text/button.blade.php ENDPATH**/ ?>
